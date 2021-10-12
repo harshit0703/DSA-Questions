bdaa76ef4298492e96b830b08d411dab442bed04
Harshit Gupta <guptas7577@gmail.com>
2021-10-12 06:15:08 +0530

    init

--- Medium/Stack/nearest_smaller_left.cpp
+++ Medium/Stack/nearest_smaller_left.cpp
@@ -5,28 +5,32 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {1,3,2,4};
+    vector<int> v = {4, 2, 3, 1};
     vector<int> ans;
     stack<int> st;
-    for(int i= v.size()-1; i>=0; i--){
+    for(int i= 0; i<v.size(); i++){
 
         // the stack is always containing the right most remaining vector for every element
         if(st.empty()){
             ans.push_back(-1);
         }
         else{
-            while(!st.empty() && st.top() <= v[i]){
+            while(!st.empty() && st.top() >= v[i]){
                 st.pop();
             }
+
+              if(st.empty()){
+            ans.push_back(-1);
+        }else{
             ans.push_back(st.top());
         }
 
-        st.push(v[i]);
+        }
+
+      st.push(v[i]);
     }
 
     // in this way we only traverse the given vector 1 time hence the optimized time compexity becomes O(n) instead of O(n^2)
-
-    reverse(ans.begin(), ans.end());
 
     for(int i:ans){
         cout<<i<<" ";
---
+++ Medium/Stack/nearest_smaller_right.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> v = {4, 2, 3, 1};
+    vector<int> ans;
+    stack<int> st;
+    for(int i=v.size()-1; i>=0; i--){
+
+        // the stack is always containing the right most remaining vector for every element
+        if(st.empty()){
+            ans.push_back(-1);
+        }
+        else{
+            while(!st.empty() && st.top() >= v[i]){
+                st.pop();
+            }
+
+              if(st.empty()){
+            ans.push_back(-1);
+        }else{
+            ans.push_back(st.top());
+        }
+
+        }
+
+      st.push(v[i]);
+    }
+
+    // in this way we only traverse the given vector 1 time hence the optimized time compexity becomes O(n) instead of O(n^2)
+    reverse(ans.begin(), ans.end());
+
+    for(int i:ans){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
