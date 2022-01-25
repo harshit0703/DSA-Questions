a8537ea8d29f9e3e97c39b8585f97cab51eb2608
Harshit Gupta <guptas7577@gmail.com>
2022-01-25 23:40:53 +0530

    init🎉

--- Revision/revision13.cpp
+++ Revision/revision13.cpp
@@ -31,6 +31,71 @@ node* createBST(node* root, int val){
 
 }
 
+bool searchBST(node* root, int key){
+    // TC = O(logN)
+    if(root == NULL){
+        return false;
+    }
+
+    if(root->data == key){
+        return true;
+    }
+
+    if(root->data < key){
+        return searchBST(root->right, key);
+    }
+
+    return searchBST(root->left, key);
+}
+
+node* deleteBST(node* root, int key){
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(key < root->data){
+        root->left = deleteBST(root->left, key);
+    }
+
+    else if(key > root->data){
+        root->right = deleteBST(root->right, key);
+    }
+
+    else{
+        // since the root is now equal to the given key we have 3 possible cases
+
+        // root is a leaf node
+
+        // if(root->left == NULL && root->right == NULL){
+        //     free(root);
+        // }
+
+        if(root->left == NULL ){
+            node* temp = root->right;
+            free(root);
+            return temp;
+        }
+
+        else if(root->right == NULL ){
+            node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        else{
+            node* temp = root->right;
+            while(temp->left != NULL){
+                temp = temp->left;  // inorder successor    
+            }
+            root->data = temp->data;
+            root->right = deleteBST(root->right, temp->data);
+        }
+    }
+
+    return root;
+
+}
+
 void printBST(node* root){
     if(root == NULL){
         return ;
@@ -47,9 +112,14 @@ int main()
     root = createBST(root, 3);
     createBST(root, 2);
     createBST(root, 1);
-    createBST(root, 4);
+    createBST(root, 6);
     createBST(root, 5);
-    printBST(root);
+    createBST(root, 7);
+    // printBST(root);
     // inorder of bst is a sorted array
+    // cout<<searchBST(root, 7);
+    printBST(root);
+    root = deleteBST(root, 1);
+    printBST(root);
     return 0;
 }
